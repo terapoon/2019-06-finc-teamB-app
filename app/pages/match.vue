eab37bf6424a75f226be943700bc61c7904ea3f2
Kazushi Kitaya <kazushikitaya@gmail.com>
2019-06-02 12:30:16 +0900

    app: update design in match.vue

--- app/pages/match.vue
+++ app/pages/match.vue
@@ -1,13 +1,72 @@
 <template>
-  <section>
-    <p>{{ match.name }}さん</p>
-    <p>{{ match.month }}月{{ match.date }}日 {{ match.slot }} {{ match.area }} 〇〇ジム</p>
-    <p>
+  <section class="bg-pink">
+    <div class="appbar">
       <img
-        v-if="match.profileImg"
         :src="`/api/uploads/${match.profileImg}`"
-        width="200">
-    </p>
+        alt=""
+        class="profile">
+      <div class="title">
+        <div class="filter grey-white">{{ match.area }} {{ match.month }}/{{ match.date }} {{ match.slot }}から</div>
+        <div class="header white">マッチしました！</div>
+      </div>
+      <img
+        src="~/assets/chat-white.svg"
+        alt=""
+        class="chat">
+    </div>
+    <div class="card-wrapper">
+      <div class="card">
+        <img
+          :src="`/api/uploads/${match.profileImg}`"
+          class="card-image"
+          alt="">
+        <div class="card-property padding-bottom">
+          <div class="card-name">{{ match.name }}</div>
+          <div class="card-age">19</div>
+          <img
+            src="~/assets/info.svg"
+            alt="">
+        </div>
+      </div>
+    </div>
+    <div class="bg-blue">
+      <div class="header white tac padding-bottom">予約詳細</div>
+      <div class="row">
+        <div class="row-left grey-white">日時</div>
+        <div class="row-right white">{{ match.month }}/{{ match.date }} {{ match.slot }}</div>
+      </div>
+      <div class="row">
+        <div class="row-left grey-white">エリア</div>
+        <div class="row-right white">{{ match.area }}</div>
+      </div>
+      <div class="card-wrapper">
+        <div class="card mb-clear">
+          <img
+            src="~/assets/school.jpeg"
+            class="card-image gym-image"
+            alt="">
+          <div class="card-property">
+            <div class="card-name gym-name">BELE MODY MAKE STUDIO</div>
+            <img
+              src="~/assets/google-maps.svg"
+              alt="">
+          </div>
+          <div class="card-button">
+            <div class="reserved">
+              <img
+                src="~/assets/check-green.svg"
+                alt="">予約済み
+            </div>
+            <div class="cancel grey-text">キャンセルする</div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="row-left grey-white">料金</div>
+        <div class="row-right white">¥1,800</div>
+        <div class="row-left grey-white paid">請求済み</div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -16,6 +75,7 @@ export default {
   data() {
     return {
       match: {},
+      me: {},
     };
   },
   async mounted() {
@@ -23,6 +83,12 @@ export default {
       this.$router.push('/login');
       return;
     }
+
+    this.me = await this.$axios.get('/me', {
+      headers: {
+        'X-AUTH-TOKEN': this.$store.state.token,
+      },
+    });
 
     this.match = await this.$axios.get('/matches', {
       headers: {
@@ -38,7 +104,7 @@ export default {
   head() {
     return {
       meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'viewport', content: '' },
       ],
     };
   },
