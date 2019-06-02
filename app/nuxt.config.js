eb64b38f0eae781964a1a965448976e13c0fa635
Kazushi Kitaya <kazushikitaya@gmail.com>
2019-06-02 10:56:28 +0900

    add stylesheet for noto sans

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -11,6 +11,7 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans+JP' },
     ],
   },
   loading: { color: '#fff' },
